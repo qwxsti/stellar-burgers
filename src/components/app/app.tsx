11c0a56a60eb5ff4af13f04730f2e8a8b6f31987
Nikita qwxsti <qwxsti@gmail.com>
2025-12-19 16:26:07 +0300

    header navlinks + routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,38 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/'>
+        <Route index element={<ConstructorPage />} />
+        <Route path='feed' element={<Feed />} />
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+        <Route path='forgot-password' element={<ForgotPassword />} />
+        <Route path='reset-password' element={<ResetPassword />} />
+        <Route path='profile'>
+          <Route index element={<Profile />} />
+          <Route path='orders' element={<ProfileOrders />} />
+        </Route>
+        <Route path='*' element={<NotFound404 />} />
+      </Route>
+    </Routes>
   </div>
 );
 
